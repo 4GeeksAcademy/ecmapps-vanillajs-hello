761350e8b1b2cf06838a9f0ff3811cce7343ddd8
ecmapps <72906286+ecmapps@users.noreply.github.com>
2023-07-06 00:26:49 +0000

    Final commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excuse").innerHTML = newExcuse();
 };
+function newExcuse() {
+  let str = "";
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class.",
+    "right on time.",
+    "when I finished.",
+    "during my lunch.",
+    "while I was praying."
+  ];
+  let words = [who, action, what, when];
+  for (let i = 0; i < words.length; i++) {
+    let randomNumber = Math.floor(Math.random() * words[i].length);
+    str += words[i][randomNumber];
+    //Adding a space between words below except to the last one.
+    if (i + 1 < words.length) {
+      str += " ";
+    }
+  }
+
+  return str;
+}
